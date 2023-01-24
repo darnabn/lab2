65ac1768056b4f4fc21e002659321c6349279357
Дарын Абен <abend13mail.ru@gmail.com>
2023-01-24 11:49:34 +0600

    first commit

---
+++ lab2.py
@@ -0,0 +1,18 @@
+print("Enter command: 1, 2, 3?")
+
+
+a = int(input())
+
+
+if(a == 1):
+    print("Your first name is Daryn")
+
+elif(a == 2):
+    print("Your last name is Aben")
+
+
+elif(a == 3):
+    print("Your age is 20")
+
+else:
+    print("Unknown command")
